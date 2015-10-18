6ccc502875a2b222d8a25dcc7d0f385e53905421
Zdeněk Solnický <zdenek.solnicky@projektove.cz>
2015-10-18 15:59:45 +0200

    chore: cleanup, express structure

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,19 @@ gulp.task('server', ['styles:scss'], function () {
   var server = plugins.express;
 
   // Start the server at the beginning of the task
-  server.run(['src/index.js']);
+  server.run(['app/app.js']);
 
 
-  gulp.watch(['src/**/*.html', 'src/**/*.jade'], server.notify);
-  gulp.watch(['src/styles/**/*.scss'], ['styles:scss']);
-  gulp.watch(['src/public/css/**/*.css'], server.notify);
-  gulp.watch(['src/scripts/**/*.js'], ['jshint']);
-  gulp.watch(['src/images/**/*'], server.notify);
-  gulp.watch(['src/index.js', 'routes/**/*.js'], [server.run]);
+  gulp.watch(['app/**/*.html', 'app/**/*.jade'], server.notify);
+  gulp.watch(['app/styles/**/*.scss'], ['styles:scss']);
+  gulp.watch(['app/public/css/**/*.css'], server.notify);
+  gulp.watch(['app/scripts/**/*.js'], ['jshint']);
+  gulp.watch(['app/images/**/*'], server.notify);
+  gulp.watch(['app/app.js', 'routes/**/*.js'], [server.run]);
 });
 
 gulp.task('styles:scss', function () {
-  return gulp.src('src/styles/**/*.scss')
+  return gulp.src('app/styles/**/*.scss')
       .pipe(plugins.sass().on('error', plugins.sass.logError))
-      .pipe(gulp.dest('src/public/css'));
+      .pipe(gulp.dest('app/public/css'));
 });
