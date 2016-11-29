4807efd14e24314ca9b905d0f1662f3c653696e3
Milan Lempera <milanlempera@gmail.com>
2016-11-29 11:47:12 +0100

    update dependencies, switch to pug

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,18 @@ gulp.task('server', ['styles:scss'], function () {
   // Start the server at the beginning of the task
   server.run(['app/app.js']);
 
-
   gulp.watch(['app/**/*.html', 'app/**/*.jade'], server.notify);
   gulp.watch(['app/styles/**/*.scss'], ['styles:scss']);
   gulp.watch(['app/public/css/**/*.css'], server.notify);
   gulp.watch(['app/scripts/**/*.js'], ['jshint']);
   gulp.watch(['app/images/**/*'], server.notify);
-  gulp.watch(['app/app.js', 'app/controllers/**/*.js', 'app/models/**/*.js', 'app/helpers/**/*.js', 'app/middlewares/**/*.js'], [server.run]);
+  gulp.watch(['app/**/*.js'], [server.run]);
 });
 
 gulp.task('styles:scss', function () {
   return gulp.src('app/styles/**/*.scss')
       .pipe(plugins.sass().on('error', plugins.sass.logError))
-      .pipe(gulp.dest('app/public/css'));
+      .pipe(gulp.dest('public/css'));
 });
 
 
