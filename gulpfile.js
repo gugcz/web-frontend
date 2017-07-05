54e0d8aff998af38a5b9f51c5f66f388b63bc0ce
Milan Lempera <milanlempera@gmail.com>
2017-07-05 16:56:31 +0200

    update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,39 @@
-// gulpfile.js
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
 
-gulp.task('server', ['styles:scss'], function() {
-  var server = plugins.express;
+gulp.task('server', ['styles:scss'], function () {
+    const liveServer = plugins.liveServer;
+    const server = liveServer.new('app/app.js');
+    server.start();
 
-  // Start the server at the beginning of the task
-  server.run(['app/app.js']);
+    const notifyListener = (...args) => server.notify(...args);
 
-  gulp.watch(['app/**/*.html', 'app/**/*.jade'], server.notify);
-  gulp.watch(['app/styles/**/*.scss'], ['styles:scss']);
-  gulp.watch(['app/public/css/**/*.css'], server.notify);
-  gulp.watch(['app/scripts/**/*.js'], ['jshint']);
-  gulp.watch(['app/images/**/*'], server.notify);
-  gulp.watch(['app/**/*.js'], [server.run]);
+    gulp.watch(['app/**/*.html', 'app/**/*.pug'], notifyListener);
+    gulp.watch(['app/styles/**/*.scss'], ['styles:scss']);
+    gulp.watch(['app/public/css/**/*.css'], notifyListener);
+    gulp.watch(['app/scripts/**/*.js'], ['jshint']);
+    gulp.watch(['app/images/**/*'], notifyListener);
+    gulp.watch(['app/**/*.js'], () => server.start());
+
+    process.on('beforeExit', () => {
+        server.stop();
+    });
 });
 
-gulp.task('styles:scss', function() {
-  return gulp.src('app/styles/**/*.scss')
-    .pipe(plugins.sass().on('error', plugins.sass.logError))
-    .pipe(gulp.dest('public/css'));
+gulp.task('styles:scss', function () {
+    return gulp.src('app/styles/**/*.scss')
+        .pipe(plugins.sass().on('error', plugins.sass.logError))
+        .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('copyscripts', function() {
-  gulp.src('./app/scripts/**/*')
-    .pipe(gulp.dest('./public/scripts'));
+gulp.task('copyscripts', function () {
+    gulp.src('./app/scripts/**/*')
+        .pipe(gulp.dest('./public/scripts'));
 });
 
-gulp.task('copyimages', function() {
-  gulp.src('./app/images/**/*')
-    .pipe(gulp.dest('./public/images'));
+gulp.task('copyimages', function () {
+    gulp.src('./app/images/**/*')
+        .pipe(gulp.dest('./public/images'));
 });
-
 
 gulp.task('build', ['styles:scss', 'copyimages', 'copyscripts']);
