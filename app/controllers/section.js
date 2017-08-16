ac78a98180d5e3e8a8c09e62c514ea40070ff45d
horm <horak@horm.cz>
2017-08-17 00:12:56 +0200

    Section page - Get chapters from firebase

--- app/controllers/section.js
+++ app/controllers/section.js
@@ -1,10 +1,13 @@
 const sectionModel = require('../models/sections');
+const chapterModel = require('../models/chapter');
 
 module.exports = async function sectionController(req, res) {
   const section = await sectionModel.getSection(req.params.sectionName);
+  const chapters = await chapterModel.getChapters(req.params.sectionName);
 
   res.render('section/detail', {
-    section
+    section,
+    chapters
   });
 };
 
--- app/models/chapter.js
+++ app/models/chapter.js
@@ -9,6 +9,18 @@ exports.getChapterInfo = async function (id) {
   return fetch(url)
     .then(responseConverterFactory(url))
 };
+
+
+exports.getChapters = async function (section) {
+
+  const url = CLOUD_FUNCTIONS_URL + 'getChapters?section=' + section;
+
+  return fetch(url)
+    .then(responseConverterFactory(url))
+};
+
+
+
 // TODO move to model helpers
 function responseConverterFactory(resourceName) {
   return function responseToJson(response) {
