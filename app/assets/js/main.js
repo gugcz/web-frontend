2a00d09dcee4f6e7195d92c20e60da30c20e911e
HormCZ <horak@horm.cz>
2018-01-29 21:26:57 +0100

    Event map - Add label style and enable label clicking

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -9,12 +9,44 @@ $(document).ready(function() {
   });
 
     events.forEach(function(event) {
-      new MarkerWithLabel({
-        position: new google.maps.LatLng(
-          event.coordinates.lat, event.coordinates.lng),
-        labelClass: 'card',
-        labelContent: '<p>'+ event.name +'</p>',
+      padding = 10;
+      div = $('<div/>')[0];
+
+      width = 210;
+      height = 10;
+      index = 0;
+
+      table = $('<div/>').addClass('card')
+        .css('position', 'relative')
+        .css('left', (index * 10) + 'px')
+        .css('top', -(index * 10) + 'px')
+        .appendTo(div);
+      tr = $('<tr/>').appendTo(table);
+      groupStyle = 'gdg';
+      $('<td/>').addClass('date ' + groupStyle).html('9' + '.').appendTo(tr);
+      $('<td/>').addClass('name').html(event.name).appendTo(tr);
+
+      width += padding;
+      height += 45;
+      index++;
+
+      div.style.width = width + 'px';
+
+      latLng = new google.maps.LatLng(
+        event.coordinates.lat, event.coordinates.lng);
+
+      marker = new MarkerWithLabel({
+        icon: ' ',
+        position: latLng,
         map: map,
+        labelContent: div,
+        labelAnchor: new google.maps.Point(width * 0.25, height * 0.25),
       });
+
+      google.maps.event.addListener(
+        marker, 'click', function(e) {
+          win = window.open('event/' + event.urlId, '_blank');
+          win.focus();
+});
     });
 });
