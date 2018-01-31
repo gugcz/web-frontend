b19d029bbeb381c7f98fcc06d68c428baee73eca
HormCZ <horak@horm.cz>
2018-01-31 11:01:18 +0100

    Event page - make sticky footer

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
   $('.parallax').parallax();
 
 
-  map = new google.maps.Map(document.getElementById('map_canvas'), {
-    zoom: 8,
-    center: new google.maps.LatLng(49.84264, 15.46619),
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-  });
+  if (google) {
+    map = new google.maps.Map(document.getElementById('map_canvas'), {
+      zoom: 8,
+      center: new google.maps.LatLng(49.84264, 15.46619),
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+    });
 
-  showEventsOnMap(events, map);
+    showEventsOnMap(events, map);
+  }
 });
