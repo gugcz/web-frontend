cb9a8fd0ce5ac8e781abfcbee484a6e198a39746
HormCZ <horak@horm.cz>
2018-01-31 10:13:38 +0100

    Extract js generating labels to mixin

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -8,45 +8,5 @@ $(document).ready(function() {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
   });
 
-    events.forEach(function(event) {
-      padding = 10;
-      div = $('<div/>')[0];
-
-      width = 210;
-      height = 10;
-      index = 0;
-
-      table = $('<div/>').addClass('card')
-        .css('position', 'relative')
-        .css('left', (index * 10) + 'px')
-        .css('top', -(index * 10) + 'px')
-        .appendTo(div);
-      tr = $('<tr/>').appendTo(table);
-      groupStyle = event.section;
-      $('<td/>').addClass('date ' + groupStyle).html(event.date).appendTo(tr);
-      $('<td/>').addClass('name').html(event.name).appendTo(tr);
-
-      width += padding;
-      height += 45;
-      index++;
-
-      div.style.width = width + 'px';
-
-      latLng = new google.maps.LatLng(
-        event.coordinates.lat, event.coordinates.lng);
-
-      marker = new MarkerWithLabel({
-        icon: ' ',
-        position: latLng,
-        map: map,
-        labelContent: div,
-        labelAnchor: new google.maps.Point(width * 0.25, height * 0.25),
-      });
-
-      google.maps.event.addListener(
-        marker, 'click', function(e) {
-          win = window.open('event/' + event.urlId, '_blank');
-          win.focus();
-});
-    });
+  showEventsOnMap(events, map);
 });
