00643281074cfb7b31362886c18a70d55f031c6a
HormCZ <horak@horm.cz>
2018-02-01 11:14:46 +0100

    Add redicters for old urls (event terms missing)

--- app/app.js
+++ app/app.js
@@ -11,7 +11,6 @@ const eventController = require('./controllers/event');
 const app = express();
 
 if (process.env.NODE_ENV !== 'production') {
-
   app.use('/public/images', express.static(__dirname + '/images'));
   app.use('/public/scripts', express.static(__dirname + '/scripts'));
   app.use('/public/fonts', express.static(__dirname + '../node_modules/materialize-css/dist/fonts/'));
@@ -20,36 +19,64 @@ if (process.env.NODE_ENV !== 'production') {
 app.set('view engine', 'pug');
 app.set('views', [__dirname + '/views/']);
 
-//app.use('/node_modules', express.static(__dirname + '/../node_modules'));
+// app.use('/node_modules', express.static(__dirname + '/../node_modules'));
 app.use('/public', express.static(__dirname + '/../public'));
+
+
+app.use('/cs/akce/:eventUrl', function(req, res) {
+  return res.redirect('/event/' + req.param('eventUrl'));
+});
+app.use('/cs/stranky/pridej-se', function(req, res) {
+  return res.redirect('/join');
+});
+
+app.use('/cs/stranky/o-nas', function(req, res) {
+  return res.redirect('/');
+});
 
 app.use('/event/:eventUrl', asyncErrorChecking(eventController));
 app.use('/events', require('./controllers/events'));
 app.use('/join', require('./controllers/join'));
 app.use('/history', require('./controllers/history'));
 
+app.use('/cs/:sectionName/skupiny/:chapterName', function(req, res) {
+  return res.redirect('/chapter/' + req.param('sectionName') + '-' + req.param('chapterName'));
+});
+
+
+app.use('/cs/:sectionName', function(req, res) {
+  return res.redirect('/section/' + req.param('sectionName'));
+});
+
+
 app.use('/section/:sectionName', asyncErrorChecking(sectionController));
 app.use('/chapter/:chapterId', asyncErrorChecking(chapterController));
+
+app.use('/cs', function(req, res) {
+  return res.redirect('/');
+});
+
+
 app.use('/', asyncErrorChecking(indexController));
+
 
 // Enable browserSync in development mode
 
 function listening(port) {
-  var browserSync = require('browser-sync');
-  var config = {
-    files: ["public/**/*.{js,css}", "app/views/**/*.pug"],
+  let browserSync = require('browser-sync');
+  let config = {
+    files: ['public/**/*.{js,css}', 'app/views/**/*.pug'],
     logLevel: 'info',
     logPrefix: 'GUG.cz frontend',
     logSnippet: false,
     reloadDelay: 100,
-    //reloadOnRestart: true,
+    // reloadOnRestart: true,
     port: port + 1,
     proxy: 'localhost:' + port,
     ui: false,
   };
-  var bs = browserSync(config);
-  //app.use(require('connect-browser-sync')(bs));
-
+  let bs = browserSync(config);
+  // app.use(require('connect-browser-sync')(bs));
 }
 
 const server = app.listen(process.env.PORT || 3000, function() {
@@ -62,5 +89,5 @@ const server = app.listen(process.env.PORT || 3000, function() {
   }
 
 
-  console.log('GUG.cz web listening at http://%s:%s', host, port)
+  console.log('GUG.cz web listening at http://%s:%s', host, port);
 });
